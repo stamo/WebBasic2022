2abef26f734946cb6e4ec97cfda228df4ffa2ca9
Стамо Петков <sgpetkov@is-bg.net>
2022-01-28 19:47:40 +0200

    HomeController implemented

--- BasicWebServer.Demo/Controllers/HomeController.cs
+++ BasicWebServer.Demo/Controllers/HomeController.cs
@@ -5,11 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace BasicWebServer.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private const string DownloadForm = @"<form action='/Content' method='POST'>
+                <input type='submit' value ='Download Sites Content' /> 
+            </form>";
+
+        private const string FileName = "content.txt";
+
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+            Name: <input type='text' name='Name'/>
+            Age: <input type='number' name ='Age'/>
+            <input type='submit' value ='Save' />
+        </form>";
+
         public HomeController(Request request)
             : base(request)
         {
@@ -17,5 +30,89 @@ namespace BasicWebServer.Demo.Controllers
         }
 
         public Response Index() => Text("Hello from the server!");
+
+        public Response Redirect() => Redirect("https://softuni.bg");
+
+        public Response Html() => Html(HtmlForm);
+
+        public Response HtmlFormPost()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var (key, value) in Request.Form)
+            {
+                sb.AppendLine($"{key} - {value}");
+            }
+
+            return Text(sb.ToString());
+        }
+
+        public Response DownloadContent() => File(FileName);
+
+        public Response Cookies()
+        {
+            var requestHasCookies = Request.Cookies.Any(c => c.Name != Server.HTTP.Session.SessionCookieName);
+            var bodyText = "";
+
+            if (requestHasCookies)
+            {
+                var cookieText = new StringBuilder();
+                cookieText.AppendLine("<h1>Cookies</h1>");
+
+                cookieText
+                    .Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
+
+                foreach (var cookie in Request.Cookies)
+                {
+                    cookieText.Append("<tr>");
+                    cookieText
+                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
+                    cookieText
+                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
+                    cookieText.Append("</tr>");
+                }
+                cookieText.Append("</table>");
+
+                bodyText = cookieText.ToString();
+
+                return Html(bodyText);
+            }
+            else
+            {
+                bodyText = "<h1>Cookies set!</h1>";
+            }
+
+            var cookies = new CookieCollection();
+
+            if (!requestHasCookies)
+            {
+                cookies.Add("My-Cookie", "My-Value");
+                cookies.Add("My-Second-Cookie", "My-Second-Value");
+            }
+
+            return Html(bodyText, cookies);
+        }
+
+        public Response Session()
+        {
+            var sessionExists = Request.Session
+                .ContainsKey(Server.HTTP.Session.SessionCurrentDateKey);
+
+            var bodyText = "";
+
+            if (sessionExists)
+            {
+                var currentDate = Request.Session[Server.HTTP.Session.SessionCurrentDateKey];
+                bodyText = $"Stored date: {currentDate}!";
+            }
+            else
+            {
+                bodyText = "Current date stored!";
+            }
+
+            return Text(bodyText);
+        }
+
+        public Response Content() => Html(DownloadForm);
     }
 }
