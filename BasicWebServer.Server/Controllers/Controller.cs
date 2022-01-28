baa8d429a928e2ce49dab01fe877c115b4ba46da
Стамо Петков <sgpetkov@is-bg.net>
2022-01-28 19:47:05 +0200

    Overload added for Html response with cookies

--- BasicWebServer.Server/Controllers/Controller.cs
+++ BasicWebServer.Server/Controllers/Controller.cs
@@ -19,6 +19,21 @@ namespace BasicWebServer.Server.Controllers
 
         protected Response Text(string text) => new TextResponse(text);
         protected Response Html(string text) => new HtmlResponse(text);
+        protected Response Html(string html, CookieCollection cookies)
+        { 
+            var response = new HtmlResponse(html);
+
+            if (cookies != null)
+            {
+                foreach (var cookie in cookies)
+                {
+                    response.Cookies.Add(cookie.Name, cookie.Value);
+                }
+            }
+
+            return response;
+        }
+
         protected Response BadRequest() => new BadRequestResponse();
         protected Response Unauthorized() => new UnauthorizedResponse();
         protected Response NotFound() => new NotFoundResponse();
