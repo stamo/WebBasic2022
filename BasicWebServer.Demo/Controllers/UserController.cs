4f375426dbe6aad5a152e99d9d3e60db07881d31
Стамо Петков <sgpetkov@is-bg.net>
2022-01-28 20:32:14 +0200

    UserController added

---
+++ BasicWebServer.Demo/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        private const string LoginForm = @"<form action='/Login' method='POST'>
+                Username: <input type='text' name='Username'/>
+                Password: <input type='password' name='Password'/>
+                <input type='submit' value ='Log In' /> 
+            </form>";
+
+        private const string Username = "user";
+
+        private const string Password = "user123";
+
+        public UserController(Request request) 
+            : base(request)
+        {
+        }
+
+        public Response LoginUser()
+        {
+            Request.Session.Clear();
+
+            var bodyText = "";
+
+            var usernameMatches = Request.Form["Username"] == Username;
+            var passwordMatches = Request.Form["Password"] == Password;
+
+            if (usernameMatches && passwordMatches)
+            {
+                Request.Session[Session.SessionUserKey] = "MyUserId";
+                CookieCollection cookies = new CookieCollection();
+                cookies.Add(Session.SessionCookieName,
+                    Request.Session.Id);
+
+                bodyText = "<h3>Logged successfully!</h3>";
+
+                return Html(bodyText, cookies);
+            }
+            else
+            {
+                bodyText = LoginForm;
+            }
+
+            return Html(bodyText);
+        }
+
+        internal Response GetUserData()
+        {
+            if (Request.Session.ContainsKey(Session.SessionUserKey))
+            {
+                return Html($"<h3>Currently logged-in user is with username '{Username}'</h3>");
+            }
+
+            return Redirect("/login");
+        }
+
+        internal Response Logout()
+        {
+            Request.Session.Clear();
+
+            return Html("<h3>Logged out successfully!</h3>");
+        }
+
+        public Response Login() => Html(LoginForm);
+    }
+}
