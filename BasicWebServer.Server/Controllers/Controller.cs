fac100443476acd266b2460355109c244cc08828
Стамо Петков <sgpetkov@is-bg.net>
2022-01-28 21:25:34 +0200

    ViewResponse added

--- BasicWebServer.Server/Controllers/Controller.cs
+++ BasicWebServer.Server/Controllers/Controller.cs
@@ -3,6 +3,7 @@ using BasicWebServer.Server.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,5 +40,11 @@ namespace BasicWebServer.Server.Controllers
         protected Response NotFound() => new NotFoundResponse();
         protected Response Redirect(string location) => new RedirectResponse(location);
         protected Response File(string fileName) => new FileResponse(fileName);
+        protected Response View([CallerMemberName] string viewName = "")
+            => new ViewResponse(viewName, GetControllerName());
+
+        private string GetControllerName() 
+            => this.GetType().Name
+                .Replace(nameof(Controller), string.Empty);
     }
 }
---
+++ BasicWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,31 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses
+{
+    public  class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controllerName)
+            : base(string.Empty, ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+
+            var viewPath = Path
+                .GetFullPath($"./Views/{viewName.TrimStart(PathSeparator)}.cshtml");
+            var viewContent = File.ReadAllText(viewPath);
+
+            Body = viewContent;
+        }
+                
+    }
+}
