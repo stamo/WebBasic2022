cd04014bf5d2b7e992a555159bb410ed83a672fb
Стамо Петков <sgpetkov@is-bg.net>
2022-02-04 20:11:36 +0200

    Query added to Request

--- BasicWebServer.Server/HTTP/Request.cs
+++ BasicWebServer.Server/HTTP/Request.cs
@@ -24,6 +24,8 @@ namespace BasicWebServer.Server.HTTP
 
         public IReadOnlyDictionary<string, string> Form { get; private set; }
 
+        public IReadOnlyDictionary<string, string> Query { get; private set; }
+
         public static IServiceCollection ServiceCollection { get; private set; }
 
         public static Request Parse(string request, IServiceCollection serviceCollection)
@@ -35,7 +37,7 @@ namespace BasicWebServer.Server.HTTP
             var startLine = lines.First().Split(" ");
 
             var method = ParseMethod(startLine[0]);
-            var url = startLine[1];
+            (string url, Dictionary<string, string> query) = ParseUrl(startLine[1]);
 
             var headers = ParseHeaders(lines.Skip(1));
 
@@ -57,8 +59,37 @@ namespace BasicWebServer.Server.HTTP
                 Cookies = cookies,
                 Body = body,
                 Session = session,
-                Form = form
+                Form = form,
+                Query = query
             };
+        }
+
+        private static (string url, Dictionary<string, string> query) ParseUrl(string queryString)
+        {
+            string url = String.Empty;
+            Dictionary<string, string> query = new Dictionary<string, string>();
+            var parts = queryString.Split("?",2);
+
+            if (parts.Length == 1)
+            {
+                url = parts[0];
+            }
+            else
+            {
+                var queryParams = parts[1].Split("&");
+
+                foreach (var pair in queryParams)
+                {
+                    var param = pair.Split('=');
+                    
+                    if (param.Length == 2)
+                    {
+                        query.Add(param[0], param[1]);
+                    }
+                }
+            }
+
+            return (url, query);
         }
 
         private static Method ParseMethod(string method)
